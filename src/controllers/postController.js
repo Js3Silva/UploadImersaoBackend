24deabd7a29579f674e7672ff4048ed45ed7dfb1
Jonathan Sena da Silva <jonathansilvagw@gmail.com>
2024-11-22 09:05:07 -0300

    uploads de imagens

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -2,10 +2,21 @@ import {getTodosPosts, criarPubli} from "../models/postModel.js";
 
 export async function listarPosts(req, res) {
     const resultado = await getTodosPosts(); // Chama a função para buscar todos os posts
-    res.status(200).json(posts); // Retorna os posts em formato JSON com status 200 (sucesso)
+    res.status(200).json(resultado); // Retorna os posts em formato JSON com status 200 (sucesso)
 }
 
 export async function postarNovaPublicacao(req, res) {
+    const novaPubli = req.body;
+    try{
+        const publiCriada = await criarPubli(novaPubli);
+        res.status(200).json(publiCriada);
+    } catch(erro) {
+        console.log(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"});
+    }
+}
+
+export async function uploadImagem(req, res) {
     const novaPubli = req.body;
     try{
         const publiCriada = await criarPubli(novaPubli);
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,24 @@
 import express from "express";
-import { listarPosts, postarNovaPublicacao } from "../controllers/postController.js";
+import multer from "multer";
+import { listarPosts, postarNovaPublicacao, uploadImagem } from "../controllers/postController.js";
+
+//somente no caso do windows
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage})
 
 const routes = (app) => {
     app.use(express.json()); // Habilita o middleware para lidar com requisições JSON
     app.get("/posts", listarPosts);
     app.post("/posts", postarNovaPublicacao);
+    app.post("/upload", upload.single("imagem"), uploadImagem);
 }
 
 export default routes;
